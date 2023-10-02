00119cb9d6100fc27cda77a23f7857dc4053db1e
GhaidaaHammad91 <77543995+GhaidaaHammad91@users.noreply.github.com>
2023-10-02 21:35:20 +0300

    Add files via upload

---
+++ DTO/Models.cs
@@ -0,0 +1,30 @@
+﻿namespace VehiclesProject.DTO
+{
+    public class Models
+    {
+        public int Make_ID { get; set; }
+        public string Make_Name { get; set; }
+        public int Model_ID { get; set; }
+        public string Model_Name { get; set; }
+    }
+
+    public class APIResponse
+    {
+        public int Count { get; set; }
+        public string? Message { get; set; }
+
+        public string? SearchCriteria { get; set; }
+
+        public List<Models> Results { get; set; }
+    }
+
+    public class ModelResponse
+    {
+        public List<string> Models { get; set;}
+
+        public ModelResponse(List<string> models)
+        {
+            this.Models = models;
+        }
+    }
+}
